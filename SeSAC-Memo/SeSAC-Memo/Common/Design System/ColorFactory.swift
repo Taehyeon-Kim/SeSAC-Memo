3d1f812097a83e09812a37d162d16e1b5bdf5227
김태현 <taehyk528@gmail.com>
2022-09-01 23:16:06 +0900

    [#11] ColorFactory Singleton 클래스 구현 - 컬러 다크모드 적용 테스트

---
+++ SeSAC-Memo/SeSAC-Memo/Common/Design System/ColorFactory.swift
@@ -0,0 +1,41 @@
+//
+//  ColorFactory.swift
+//  SeSAC-Memo
+//
+//  Created by taekki on 2022/09/01.
+//
+
+import UIKit
+
+final class ColorFactory {
+
+    static let shared = ColorFactory()
+    
+    private init() {}
+    
+    enum ColorStyle {
+        case primary
+        case delete
+        case background // white, black
+        case neutral200
+        case neutral400
+        case neutral600
+    }
+    
+    func create(_ style: ColorStyle) -> UIColor {
+        switch style {
+        case .primary:
+            return .systemOrange
+        case .delete:
+            return .systemRed
+        case .background:
+            return .systemBackground
+        case .neutral200:
+            return .systemGray6
+        case .neutral400:
+            return .systemGray4
+        case .neutral600:
+            return .systemGray2
+        }
+    }
+}
