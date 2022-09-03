80b2a1bfa01daeb4d4bbc3823f57d492dbd39b30
김태현 <taehyk528@gmail.com>
2022-09-03 17:52:46 +0900

    [#16] 메모 리스트 화면 레이아웃 가이드라인 잡기 - UISearchViewController 적용 - UIToolbar 적용 - UITableView 적용

--- SeSAC-Memo/SeSAC-Memo/Presentation/MemoList/MemoListViewController.swift
+++ SeSAC-Memo/SeSAC-Memo/Presentation/MemoList/MemoListViewController.swift
@@ -9,11 +9,67 @@ import UIKit
 
 final class MemoListViewController: BaseViewController {
     
+    private let repository = MemoRepository()
+    
+    private let rootView = MemoListView()
+    
+    override func loadView() {
+        self.view = rootView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureSearchController()
+        configureTableView()
     }
     
     override func configureAttributes() {
         view.backgroundColor = ColorFactory.shared.create(.background)
+    }
+}
+
+extension MemoListViewController {
+    
+    private func configureSearchController() {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "검색"
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        self.navigationItem.searchController = searchController
+        self.navigationItem.title = "Count"
+        self.navigationItem.hidesSearchBarWhenScrolling = true
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureTableView() {
+        self.rootView.tableView.delegate = self
+        self.rootView.tableView.dataSource = self
+    }
+}
+
+extension MemoListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MemoCell.reuseIdentifier, for: indexPath) as? MemoCell else {
+            return UITableViewCell()
+        }
+        
+        // MemoProtocol을 채택하는 객체를 주입해주려고 하는데, 이게 맞는 방식인지 모르겠음.
+        let memo: MemoProtocol = MemoInterface(
+            title: "title",
+            content: "content"
+        )
+        cell.configure(with: memo)
+        
+        return cell
     }
 }
--- SeSAC-Memo/SeSAC-Memo/Presentation/MemoList/View/MemoListView.swift
+++ SeSAC-Memo/SeSAC-Memo/Presentation/MemoList/View/MemoListView.swift
@@ -13,19 +13,38 @@ import Then
 final class MemoListView: BaseView {
     
     lazy var tableView = UITableView(frame: .zero, style: .insetGrouped)
+    private let toolbar = UIToolbar(
+        frame: CGRect(
+            origin: .zero,
+            size: CGSize(width: 100, height: 44)
+        )
+    )
     
     override func configureAttributes() {
         tableView.do {
             $0.backgroundColor = .clear
             $0.register(MemoCell.self, forCellReuseIdentifier: MemoCell.reuseIdentifier)
         }
+        
+        toolbar.do {
+            let writeItem = UIBarButtonItem(image: UIImage(systemName: "square.and.pencil"), style: .plain, target: nil, action: nil)
+            let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+            $0.backgroundColor = ColorFactory.shared.create(.background)
+            $0.tintColor = ColorFactory.shared.create(.primary)
+            $0.setItems([flexibleSpace, writeItem], animated: true)
+        }
     }
     
     override func configureLayout() {
-        addSubview(tableView)
+        addSubviews(tableView, toolbar)
         
         tableView.snp.makeConstraints {
             $0.edges.equalTo(self.safeAreaLayoutGuide)
+        }
+        
+        toolbar.snp.makeConstraints {
+            $0.trailing.bottom.leading.equalTo(self.safeAreaLayoutGuide)
+            $0.height.equalTo(44)
         }
     }
 }
